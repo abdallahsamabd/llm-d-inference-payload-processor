d343f14e736c51f9831084d410cb921ffd368549
Senan Zedan <szedan@redhat.com>
2026-07-05 23:31:38 +0300

    Wire model-config-datasource into the runner. (#175) (#199)
    * Wire model-config-datasource into the runner and helm chart.
    
    
    
    * Add documentation.
    
    
    
    * Keep registration.
    
    
    
    ---------
    
    Signed-off-by: Mohammad <mohammad.nassar@ibm.com>
    Signed-off-by: szedan <szedan@redhat.com>
    Co-authored-by: Mohammad-nassar10 <79787844+Mohammad-nassar10@users.noreply.github.com>

--- pkg/handlers/server.go
+++ pkg/handlers/server.go
@@ -50,13 +50,15 @@ const (
 )
 
 func NewServer(preProcessors []requesthandling.RequestProcessor, profilePicker requesthandling.ProfilePicker,
-	profiles map[string]*requesthandling.Profile, postProcessors []requesthandling.ResponseProcessor) *Server {
+	profiles map[string]*requesthandling.Profile, postProcessors []requesthandling.ResponseProcessor,
+	responseHeadersPostProcessors []requesthandling.ResponseHeadersProcessor) *Server {
 	return &Server{
-		preProcessors:  preProcessors,
-		profilePicker:  profilePicker,
-		profiles:       profiles,
-		postProcessors: postProcessors,
-		emptyProfile:   requesthandling.NewProfile(),
+		preProcessors:                 preProcessors,
+		profilePicker:                 profilePicker,
+		profiles:                      profiles,
+		postProcessors:                postProcessors,
+		responseHeadersPostProcessors: responseHeadersPostProcessors,
+		emptyProfile:                  requesthandling.NewProfile(),
 	}
 }
 
@@ -69,13 +71,14 @@ func (s *Server) WithEventNotifier(n datasource.EventNotifier) *Server {
 // Server implements the Envoy external processing server.
 // https://www.envoyproxy.io/docs/envoy/latest/api-v3/service/ext_proc/v3/external_processor.proto
 type Server struct {
-	preProcessors  []requesthandling.RequestProcessor
-	profilePicker  requesthandling.ProfilePicker
-	profiles       map[string]*requesthandling.Profile
-	postProcessors []requesthandling.ResponseProcessor
-	eventNotifier  datasource.EventNotifier
+	preProcessors                 []requesthandling.RequestProcessor
+	profilePicker                 requesthandling.ProfilePicker
+	profiles                      map[string]*requesthandling.Profile
+	postProcessors                []requesthandling.ResponseProcessor
+	responseHeadersPostProcessors []requesthandling.ResponseHeadersProcessor
+	eventNotifier                 datasource.EventNotifier
 
-	emptyProfile *requesthandling.Profile // requests are always initialized with empty profile to avoid nil pointers
+	emptyProfile *requesthandling.Profile
 }
 
 // RequestContext stores context information during the lifetime of an HTTP request.
@@ -179,7 +182,7 @@ func (s *Server) Process(srv extProcPb.ExternalProcessor_ProcessServer) error {
 		case *extProcPb.ProcessingRequest_RequestTrailers:
 			responses, err = s.HandleRequestTrailers(v.RequestTrailers)
 		case *extProcPb.ProcessingRequest_ResponseHeaders:
-			responses = s.HandleResponseHeaders(ctx, reqCtx, v.ResponseHeaders)
+			responses, err = s.HandleResponseHeaders(ctx, reqCtx, v.ResponseHeaders)
 			loggerVerbose.Info("processing response headers complete")
 		case *extProcPb.ProcessingRequest_ResponseBody:
 			loggerVerbose.Info("Incoming response body chunk", "EoS", v.ResponseBody.EndOfStream)

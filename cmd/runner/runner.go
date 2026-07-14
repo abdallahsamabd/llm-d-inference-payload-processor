e0a2fb3968770c6879350ff42cb6022fc16ffeb9
Ronen Kat <16743404+ronenkat@users.noreply.github.com>
2026-07-14 16:42:37 +0300

    Add a group model filter (#227)
    * Add a group model filter
    
    Signed-off-by: ronenkat <16743404+ronenkat@users.noreply.github.com>
    
    * Bug fix: allow model names in model field
    
    Signed-off-by: ronenkat <16743404+ronenkat@users.noreply.github.com>
    
    * add support for model name filter
    
    Signed-off-by: ronenkat <16743404+ronenkat@users.noreply.github.com>
    
    * address review
    
    Signed-off-by: ronenkat <16743404+ronenkat@users.noreply.github.com>
    
    * fix typo
    
    Signed-off-by: ronenkat <16743404+ronenkat@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: ronenkat <16743404+ronenkat@users.noreply.github.com>

--- cmd/runner/runner.go
+++ cmd/runner/runner.go
@@ -50,6 +50,7 @@ import (
 	modelconfigcollector "github.com/llm-d/llm-d-inference-payload-processor/pkg/framework/plugins/datalayer/modelconfigcollector"
 	requestcostmetadata "github.com/llm-d/llm-d-inference-payload-processor/pkg/framework/plugins/datalayer/requestcostmetadata"
 	requestmetadata "github.com/llm-d/llm-d-inference-payload-processor/pkg/framework/plugins/datalayer/requestmetadata"
+	"github.com/llm-d/llm-d-inference-payload-processor/pkg/framework/plugins/modelselector/filter/modelgroup"
 	"github.com/llm-d/llm-d-inference-payload-processor/pkg/framework/plugins/modelselector/filter/modelname"
 	"github.com/llm-d/llm-d-inference-payload-processor/pkg/framework/plugins/modelselector/picker/maxscore"
 	"github.com/llm-d/llm-d-inference-payload-processor/pkg/framework/plugins/modelselector/picker/random"
@@ -298,6 +299,7 @@ func (r *Runner) registerInTreePlugins() {
 	plugin.Register(modelconfigcollector.PluginType, modelconfigcollector.DatasourceFactory)
 	// register model selector plugins
 	plugin.Register(modelname.ModelNameFilterType, modelname.ModelNameFilterFactory)
+	plugin.Register(modelgroup.ModelGroupFilterType, modelgroup.ModelGroupFilterFactory)
 	plugin.Register(random.RandomPickerType, random.RandomPickerFactory)
 	plugin.Register(maxscore.MaxScorePickerType, maxscore.MaxScorePickerFactory)
 	plugin.Register(weightedrandom.WeightedRandomPickerType, weightedrandom.WeightedRandomPickerFactory)
